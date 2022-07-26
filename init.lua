acbc9290574fa5b3abe6e41c8725a987fd7e79df
Dennis Lam <denwt.lam@outlook.com>
2022-07-26 19:48:10 -0400

    Initial setup done

---
+++ init.lua
@@ -0,0 +1,47 @@
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.softtabstop = 2
+vim.opt.expandtab = true
+vim.wo.number = true
+
+require('packer').startup(function(use)
+  -- Packer can manage itself
+  use 'wbthomason/packer.nvim'
+
+  -- Colorscheme
+  use 'joshdick/onedark.vim'
+
+  -- LSP
+  use {
+    'VonHeikemen/lsp-zero.nvim',
+    requires = {
+      -- LSP Support
+      {'neovim/nvim-lspconfig'},
+      {'williamboman/nvim-lsp-installer'},
+
+      -- Autocomplete
+      {'hrsh7th/nvim-cmp'},
+      {'hrsh7th/cmp-buffer'}, 
+      {'hrsh7th/cmp-path'},
+      {'saadparwaiz1/cmp_luasnip'},
+      {'hrsh7th/cmp-nvim-lsp'},
+      {'hrsh7th/cmp-nvim-lua'},
+
+      -- Snippets
+      {'L3MON4D3/LuaSnip'},
+      {'rafamadriz/friendly-snippets'},
+    }
+
+  }
+
+end)
+
+vim.opt.signcolumn = 'yes'
+vim.opt.termguicolors = true
+pcall(vim.cmd, 'colorscheme onedark')
+
+local lsp = require('lsp-zero')
+
+lsp.preset('recommended')
+lsp.nvim_workspace()
+lsp.setup()
